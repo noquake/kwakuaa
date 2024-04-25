64b473671e00df39286d3e94e7c1e7e952d0db65
kwaku <99109611+noquake@users.noreply.github.com>
2024-04-24 20:21:49 -0400

    starting the theme toggle feature

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+/* Theme toggling feature */
+const themeToggle = document.getElementById('theme-toggle');
+const themeLink = document.getElementById('theme');
+
+themeToggle.addEventListener('click', function() {
+    if (themeLink.getAttribute('href') === 'styles.css') {
+        themeLink.setAttribute('href', 'dark-theme.css');
+    } else {
+        themeLink.setAttribute('href', 'styles.css');
+    }
+});
