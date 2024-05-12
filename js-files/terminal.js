06c1db77fe8f8398dc3cb23858c436e6b5371cc1
kwaku <99109611+noquake@users.noreply.github.com>
2024-05-12 01:12:59 -0400

    aligned prompt to input box
    consistent output upon commands, need formatting changes

--- js-files/terminal.js
+++ js-files/terminal.js
@@ -7,35 +7,35 @@ const input = document.querySelector("input");
 const commands = {
     'help': 'List of available commands: help, about, contact',
     'about': 'This is a demo terminal interface.',
-    'contact': 'You can contact us at example@example.com.'
+    'contact': 'You can contact us at example@example.com.',
+    'clear': 'qi3hbf'
   };
 
-// terminalInput.addEventListener('keydown', function(event) {
-    terminalInput.addEventListener("keydown", (event) => {
-    if (event.key === 'Enter') { // Enter key
-      const input = terminalInput.value.trim(); // Trim any leading/trailing whitespace
-      if (input !== '') { // Check if input is not empty
+// terminalInput.addEventListener('keypress', function(event) {
+  terminalInput.addEventListener("keydown", (event) => {
+    if (event.key === 'Enter') {
+      const input = terminalInput.value.trim();
+      if (input !== '') {
         const output = executeCommand(input);
         appendToTerminal(output);
       }
-      terminalInput.value = ''; // Clear the input field after processing
+      terminalInput.value = ''; // clears input field after entering a value
     }
   });
-  
-  function executeCommand(input) {
-    const command = input.split(' ')[0]; // Extract the command (first word)
-    // const args = input.substring(command.length).trim(); // Extract any arguments after the command
-  
-    if (commands.hasOwnProperty(command)) {
-      return commands[command];
-    } else {
-      return 'Command not found: "' + command + '" ' ;
-    }
-  }
 
-function appendToTerminal(output) {
-  const newLine = document.createElement('div');
-  newLine.textContent = output;
-  terminalBody.appendChild(newLine);
-  terminalBody.scrollTop = terminalBody.scrollHeight;
-}
+    function executeCommand(input) {
+      const command = input.split(' ')[0].toLowerCase(); // Extract the command (first word)
+      const args = input.substring(command.length).trim(); // Extract any arguments after the command
+      if (commands.hasOwnProperty(command)) {
+        return commands[command];
+      } else {
+        return `Command not found: "${command}"`;
+      }
+    }
+
+    function appendToTerminal(output) {
+      const newLine = document.createElement('div');
+      newLine.textContent = output;
+      terminalBody.appendChild(newLine);
+      terminalBody.scrollTop = terminalBody.scrollHeight;
+    }
