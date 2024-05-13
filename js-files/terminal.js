185c0cfa704b4cf0b415b07fd6a4f4603eea6220
kwaku <99109611+noquake@users.noreply.github.com>
2024-05-12 21:14:12 -0400

    consistent output, commands still not working

--- js-files/terminal.js
+++ js-files/terminal.js
@@ -1,31 +1,31 @@
-const terminalBody = document.getElementById('terminal-body');
+const terminalOutput = document.getElementById('terminal-output');
 const terminalInput = document.getElementById('terminal-input');
-
 const input = document.querySelector("input");
 
 // Object to store predefined commands and their responses
 const commands = {
-    'help': 'List of available commands: help, about, contact',
+    'help': 'List of commands: help, about, contact',
     'about': 'This is a demo terminal interface.',
-    'contact': 'You can contact us at example@example.com.',
-    'clear': 'qi3hbf'
+    'contact': 'Email: kwakuagyapong478@gmail.com \nGitHub: https://github.com/noquake \nLinkedIn: https://www.linkedin.com/in/kwakuaagyapong/ \nNeed a meeting? try my Calendly https://calendly.com/kwakuagyapong478/30min',
+    'clear': terminalOutput.textContent = '',
+    'ls': 'A \nBunch \nOf \nFake \nFiles',
   };
 
-// terminalInput.addEventListener('keypress', function(event) {
   terminalInput.addEventListener("keydown", (event) => {
     if (event.key === 'Enter') {
       const input = terminalInput.value.trim();
-      if (input !== '') {
-        const output = executeCommand(input);
-        appendToTerminal(output);
+      if (input !== '') { 
+        appendToTerminal(executeCommand(input)); 
+        const currentOutput = terminalInput.parentElement;
+        currentOutput.terminalContainer.appendChild(terminalInput.parentElement); // Append it to the end
+        terminalInput.value = ''; // clears input field after entering a value
       }
-      terminalInput.value = ''; // clears input field after entering a value
     }
   });
 
     function executeCommand(input) {
       const command = input.split(' ')[0].toLowerCase(); // Extract the command (first word)
-      const args = input.substring(command.length).trim(); // Extract any arguments after the command
+    //  const args = input.substring(command.length).trim(); // Extract any arguments after the command
       if (commands.hasOwnProperty(command)) {
         return commands[command];
       } else {
@@ -36,6 +36,6 @@ const commands = {
     function appendToTerminal(output) {
       const newLine = document.createElement('div');
       newLine.textContent = output;
-      terminalBody.appendChild(newLine);
-      terminalBody.scrollTop = terminalBody.scrollHeight;
+      terminalOutput.appendChild(newLine);
+      terminalOutput.scrollTop = terminalOutput.scrollHeight;
     }
