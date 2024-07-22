97920d75d27351b7f9f1c8500ab48a8eea2bd853
kwaku <99109611+noquake@users.noreply.github.com>
2024-07-21 22:50:09 -0400

    lotta changes + refinement
    - removed the button, it was dumb
    - restructured the front page (simpler)
    + comingsoon pages for new features

--- js-files/app.js
+++ js-files/app.js
@@ -1,20 +1,7 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 });
-
-/* Theme toggling feature */
-const themeToggle = document.getElementById('theme-toggle');
-const themeLink = document.getElementById('theme');
-
-themeToggle.addEventListener('click', function() {
-    if (themeLink.getAttribute('href') === 'styles.css') {
-        themeLink.setAttribute('href', 'dark-theme.css');
-    } else {
-        themeLink.setAttribute('href', 'styles.css');
-    }
-});
-
